d2b17ea8c652257065723252320901653ad2bf0d
vlad6711 <asafevv0905@gmail.com>
2025-03-17 15:44:20 +0300

    added output file

--- src/Test3.java
+++ src/Test3.java
@@ -1,39 +1,43 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Test3 {
     public static void main(String[] args) throws FileNotFoundException {
         String separator = File.separator;
-        String path = "C:" + separator + "Users" + separator + "Владислав" + separator + "IdeaProjects" + separator + "calculator" + separator + "input.txt";
+        String path1 = "C:" + separator + "Users" + separator + "Владислав" + separator + "IdeaProjects" + separator + "calculator" + separator + "input.txt";
+        String path2 = "C:" + separator + "Users" + separator + "Владислав" + separator + "IdeaProjects" + separator + "calculator" + separator + "output.txt";
 
-        File file = new File(path);
+        File file1 = new File(path1);
+        File file2 = new File(path2);
 
-        Scanner sc = new Scanner(file);
+        Scanner sc = new Scanner(file1);
+        PrintWriter pw = new PrintWriter(file2);
 
-        while (sc.hasNextLine()) {
-            try {
-                String input = sc.nextLine();
-                String[] str = input.split(" ");
-                String a1 = str[0];
-                String sign = str[1];
-                if (!sign.equals("+") && !sign.equals("-") && !sign.equals("*") && !sign.equals("/")) throw new Exception("Operation Error!");
-                String b1 = str[2];
-                double a = Double.parseDouble(a1);
-                double b = Double.parseDouble(b1);
+        try {
+            String input = sc.nextLine();
+            String[] str = input.split(" ");
+            String a1 = str[0];
+            String sign = str[1];
+            if (!sign.equals("+") && !sign.equals("-") && !sign.equals("*") && !sign.equals("/"))
+                throw new Exception("Operation Error!");
+            String b1 = str[2];
+            double a = Double.parseDouble(a1);
+            double b = Double.parseDouble(b1);
 
-                if (sign.equals("/") && b == 0) throw new Exception("Error! Division by zero");
+            if (sign.equals("/") && b == 0) throw new Exception("Error! Division by zero");
 
-                if (sign.equals("+")) System.out.println(a + b);
-                else if (sign.equals("-")) System.out.println(a - b);
-                else if (sign.equals("*")) System.out.println(a * b);
-                else System.out.println(a / b);
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Not number");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+            if (sign.equals("+")) pw.println(String.valueOf(a + b));
+            else if (sign.equals("-")) pw.println(String.valueOf(a - b));
+            else if (sign.equals("*")) pw.println(String.valueOf(a * b));
+            else pw.println(String.valueOf(a / b));
+        } catch (NumberFormatException e) {
+            System.out.println("Error! Not number");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         sc.close();
+        pw.close();
     }
 }
